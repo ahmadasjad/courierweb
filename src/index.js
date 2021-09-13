2b93c0e2f353e954f0a9c18720b98bc929f137e0
ask2mahtab <ask2mahtab@gmail.com>
2021-09-13 15:12:50 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -4,14 +4,21 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import * as serviceWorker from './serviceWorker';
+import { hydrate, render } from "react-dom";
 
-ReactDOM.render(
-  <StrictMode>
-    <ColorModeScript />
-    <App />
-  </StrictMode>,
-  document.getElementById('root')
-);
+const rootElement = document.getElementById("root");
+let AppCompoent=<StrictMode>
+                   <ColorModeScript />
+                    <App />
+                </StrictMode>
+
+if (rootElement.hasChildNodes()) {
+  hydrate(AppCompoent, rootElement);
+} else {
+  render(AppCompoent, rootElement);
+}
+
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
