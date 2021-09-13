9c2e3eda5908dfd5e9b4caa99094fac4114b369a
ask2mahtab <ask2mahtab@gmail.com>
2021-09-13 19:22:41 +0530

    added: helmet

--- src/App.js
+++ src/App.js
@@ -3,13 +3,18 @@ import {
   ChakraProvider,
   theme,
 } from '@chakra-ui/react';
-
+import {Helmet} from "react-helmet";
 import logo from './assets/public/moveitlogo.png'
 
 
 function App() {
   return (
     <ChakraProvider theme={theme}>
+       <Helmet>
+                <meta charSet="utf-8" />
+                <title>Courier GO</title>
+                <meta name="keywords" content="courier,send parsel,dtdc price"/>
+            </Helmet>
       <main>
           <header>
               <img src={logo} alt="Move It Logo" />
