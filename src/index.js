7861816406afa2e4795331e1b13af0fd1e00dcf2
ask2mahtab <ask2mahtab@gmail.com>
2021-09-13 19:01:07 +0530

    added react dom

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import { ColorModeScript } from '@chakra-ui/react';
 import React, { StrictMode } from 'react';
+import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import * as serviceWorker from './serviceWorker';
