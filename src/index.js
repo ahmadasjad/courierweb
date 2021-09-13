3c81e494e074df82030347af03f4db71f48b89b3
ask2mahtab <ask2mahtab@gmail.com>
2021-09-13 19:01:29 +0530

    fixed: react dom

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import { ColorModeScript } from '@chakra-ui/react';
 import React, { StrictMode } from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import * as serviceWorker from './serviceWorker';
