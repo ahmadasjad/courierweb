fa02fe6da493b07b828c43980fb2116c102e1b00
ask2mahtab <ask2mahtab@gmail.com>
2021-09-16 01:52:33 +0530

    added all routes

--- src/routes/config.routes.js
+++ src/routes/config.routes.js
@@ -2,6 +2,12 @@ import {lazy} from 'react';
 const HomePage=lazy(()=>import('../pages/home.page'));
 const ServicePage=lazy(()=>import('../pages/services.page'));
 const AboutPage=lazy(()=>import('../pages/aboutus.page'))
+const GallaryPage=lazy(()=>import('../pages/gallery.page'))
+const VideoPage=lazy(()=>import('../pages/video.page'))
+const ContactusPage=lazy(()=>import('../pages/contactus.page'))
+const TestimonialPage=lazy(()=>import('../pages/testimonial.page'))
+const BlogsPage=lazy(()=>import('../pages/blog.page'))
+
 const LoginPage=lazy(()=>import('../pages/login.page'));
 const DashboardPage=lazy(()=>import('../pages/dashboard.page'))
 const RoutesConfig=[
@@ -21,6 +27,36 @@ const RoutesConfig=[
         exact:true,
         restricted:false,
         component:AboutPage
+    },
+     {
+        path:'/gallery',
+        exact:true,
+        restricted:false,
+        component:GallaryPage
+    },
+     {
+        path:'/videos',
+        exact:true,
+        restricted:false,
+        component:VideoPage
+    },
+     {
+        path:'/contactus',
+        exact:true,
+        restricted:false,
+        component:ContactusPage
+    },
+     {
+        path:'/testimonials',
+        exact:true,
+        restricted:false,
+        component:TestimonialPage
+    },
+    {
+        path:'/blogs',
+        exact:true,
+        restricted:false,
+        component:BlogsPage
     },
     {
         path:'/login',
