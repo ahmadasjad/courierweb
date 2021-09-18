714db23c658406271117d4ef6085b522b1d51823
ask2mahtab <ask2mahtab@gmail.com>
2021-09-18 10:52:24 +0530

    added: Enzyme and snap shot test for app.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import App from './App';
 import { create } from 'react-test-renderer';
-import {shallow} from 'enzyme';
-////enjyme
-const components=shallow(<App/>)
 
-/////snapshot
-describe('My first snapshot test',()=>{
-    test('testing childs', () => {
-    let tree = create(<App />)
-    expect(tree.toJSON()).toMatchSnapshot();
-  })
+import Adapter from 'enzyme-adapter-react-16';
+import {shallow,configure} from 'enzyme';
+////enjyme
+configure({adapter: new Adapter()});
+describe("Given App structure",()=>{
+    test("Then Routes should be rendred",()=>{
+            const components=shallow(<App/>)
+            // console.log(components.debug())
+            const routesExist=components.find('Routes').exists();
+            expect(routesExist).toBe(true)
+            expect(components).toMatchSnapshot();
+    })
 })
